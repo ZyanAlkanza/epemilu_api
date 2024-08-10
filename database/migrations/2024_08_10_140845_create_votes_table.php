197213261d1caf1ce3db99ab9f676017c42ee8aa
ZyanAlkanza <zyan.mujaddid@gmail.com>
2024-08-10 21:16:27 +0700

    membuat candidates & votes migration

---
+++ database/migrations/2024_08_10_140845_create_votes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->char('nik', 16);
+            $table->foreign('nik')->references('nik')->on('voters');
+            $table->foreignId('candidate_id')->constrained();
+            $table->year('tahun_pemilihan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('votes_');
+    }
+};
